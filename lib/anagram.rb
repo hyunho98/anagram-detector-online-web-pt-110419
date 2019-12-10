eb2dfe9ccffee73b5d5648e6f3edd65ba22d43a0
Alex Lee <hyunho98@gmail.com>
2019-12-10 23:17:21 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -5,3 +5,13 @@ class Anagram
   def initialize(aWord)
     @aWord = aWord
   end
+
+  def match(array)
+    matchWords = []
+    array.each{|i|
+      if @aWord.split("").sort == i.split("").sort
+        matchWords << i
+      end
+    }
+  end
+end
